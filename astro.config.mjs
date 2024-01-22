86167be023ee351ecc1caf11dd115bcd25db26b9
Brett Shollenberger <brettshollenberger@Bretts-MacBook-Pro-2.local>
2024-01-21 20:43:14 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://cool-strudel-191584.netlify.app",
   integrations: [tailwind(), icon()],
+  output: "server",
+  adapter: netlify(),
 });
